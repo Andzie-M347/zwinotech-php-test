432ef7a406dde1c043e19fa45e7ed483dd274ad7
Andzisi Mabaso <andzisishine@gmail.com>
2022-04-17 17:44:55 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP &mdash; Excel Dataset Task </title>
+</head>
+<body>
+
+    <h2> Upload Excel Dataset Task </h2>
+
+    <form action="">
+        <input type="file" name="excel_file" >
+        <button type="submit" name="excel_import">Upload</button>
+    </form>
+</body>
+</html>
